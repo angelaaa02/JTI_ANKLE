bdbf14a34ced0cb0e132ebd2c06e5253bcc16ea0
angelaaa02 <oyangela1017@gmail.com>
2024-05-22 16:51:24 +1000

    Add buzzer function

--- load_setup.ino
+++ load_setup.ino
@@ -1,20 +1,27 @@
-// Hx711.DOUT - pin #A1
-// Hx711.SCK - pin #A0
-
 #include <Hx711.h>
 
+// Hx711.DOUT - pin #A1
+// Hx711.SCK - pin #A0
 Hx711 scale(A1, A0);
+const int buzzerPin = 8; // Buzzer connected to digital pin 8
 
 void setup() {
-
-  Serial.begin(9600);
-
+  Serial.begin(9600); // Initialize serial communication with the computer and OpenLog Artemis
+  pinMode(buzzerPin, OUTPUT); // Set buzzer pin as output
 }
 
 void loop() {
+  float weight = scale.getGram();
 
-  Serial.print(scale.getGram(), 1);
+  if (weight < -20) {
+    digitalWrite(buzzerPin, HIGH); // Turn buzzer on
+  } else {
+    digitalWrite(buzzerPin, LOW); // Turn buzzer off
+  }
+
+  Serial.print("Weight: ");
+  Serial.print(weight, 1);
   Serial.println(" g");
 
-  delay(200);
+  delay(10);
 }
